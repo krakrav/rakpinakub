a9f116208dbe9c8f9f7ac4a4cd11cf5a1ebb996d
krakrav <spicysomtum0@gmail.com>
2024-12-29 03:41:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,6 +4,13 @@ local a = 2185+2055 -- 4240
       local d = 9552+1220 -- 10772
         local e = 7022+2055 -- 9077
           result =  a+b+c+d+e
-            if result == '51802' then print "Correct result" else print "Incorrect result" end
+            if result == '51,802' then print "Correct result" else print "Incorrect result" end
               local llllIllllIIIIIIlllllIIIIIllllllIIIIIllllIIIllllIIILlllIIILlll = 'llilllIIIlllIIIIiiillliiiIIII'
                 if llllIllllIIIIIIlllllIIIIIllllllIIIIIllllIIIllllIIILlllIIILlll == 'llilllIIIlllIIIIiiillliiiIIII' then print(result) else print("Incorrect result") end
+                    print(0.2,-math.huge+result,"%")
+                            print(0.1,-math.huge+result,"%")
+                            print(0.2,math.huge+result,"%")
+                            print(0.3,-math.huge+result,"%")
+                            print(0.4,math.huge+result,"%")
+                            print(0.5,-math.huge+result,"%")
+                                    warn('Stopped')
