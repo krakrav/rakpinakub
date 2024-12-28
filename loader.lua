2ff228a4e55c523844025dd06d22b1a25ad4650e
krakrav <spicysomtum0@gmail.com>
2024-12-29 03:33:43 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,18 +1,9 @@
-# Build and Release Folders
-bin-debug/
-bin-release/
-[Oo]bj/
-[Bb]in/
-
-# Other files and folders
-.settings/
-
-# Executables
-*.swf
-*.air
-*.ipa
-*.apk
-
-# Project files, i.e. `.project`, `.actionScriptProperties` and `.flexProperties`
-# should NOT be excluded as they contain compiler settings and other important
-# information for Eclipse / Flash Builder.
+local a = 2185+2055 -- 4240
+  local b = 5914+5955 -- 11869
+    local c = 5922+9922 -- 15844
+      local d = 9552+1220 -- 10772
+        local e = 7022+2055 -- 9077
+          result =  a+b+c+d+e
+            if result == '51802' then print "Correct result" else print "Incorrect result" end
+              local llllIllllIIIIIIlllllIIIIIllllllIIIIIllllIIIllllIIILlllIIILlll = 'llilllIIIlllIIIIiiillliiiIIII'
+                if llllIllllIIIIIIlllllIIIIIllllllIIIIIllllIIIllllIIILlllIIILlll == 'llilllIIIlllIIIIiiillliiiIIII' then print(result) else print("Incorrect result") end
